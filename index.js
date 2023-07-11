5be7f2b5d6ef283f3ced40f28d8baa65c72430a0
NadiaArroyo25 <niac2596@gmail.com>
2023-07-10 19:50:19 -0600

    nuevo proyecto

---
+++ index.js
@@ -0,0 +1,6 @@
+
+function 
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  returnnew bootstrap.Tooltip(tooltipTriggerEl)
+})
